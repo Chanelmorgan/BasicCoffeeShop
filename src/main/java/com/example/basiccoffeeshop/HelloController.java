4dc3e7ab9e329d82b87a8abc24a7c6bf431e6623
Chanel Morgan <chanelmorgan05@icloud.com>
2024-04-08 13:15:40 +0100

    Allowing the user to select the coffee option

--- src/main/java/com/example/basiccoffeeshop/HelloController.java
+++ src/main/java/com/example/basiccoffeeshop/HelloController.java
@@ -1,14 +1,58 @@
 package com.example.basiccoffeeshop;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private RadioButton radCoffee;
+
+    @FXML
+    private RadioButton radCapp;
+
+    @FXML
+    private RadioButton radTea;
+
+    @FXML
+    private CheckBox chkBag;
+
+    @FXML
+    private CheckBox chkCro;
+
+    @FXML
+    private CheckBox chkTo;
+
+    @FXML
+    private TextField txtSub;
+
+    @FXML
+    private TextField txtTax;
+
+    @FXML
+    private TextField txtTotal;
+
+    @FXML
+    private Button btnEnt;
+
+    @FXML
+    private Button btnExit;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    public void radCoffeeActionPerformed(ActionEvent event) {
+        // Select coffee option
+        if(radCoffee.isSelected()) {
+            radCapp.setSelected(false);
+            radTea.setSelected(false);
+        }
     }
+
+
+
 }
